77120053e260c5057d715441c0707ab7da278ad1
amiraElmergawy <amiraelmergawy@gmail.com>
2022-03-19 06:36:46 +0200

    Create DaosFactory.java
    Add DaosFactory to provide implementation to the services and easily call required services

---
+++ src/main/java/gov/iti/jets/persistance/util/DaosFactory.java
@@ -0,0 +1,12 @@
+package gov.iti.jets.persistance.util;
+
+public enum DaosFactory {
+
+    INSTANCE;
+
+    //private ExampleDao exmapleDao = new ExampleDaoImpl();
+
+    // public ExampleDao getExampleDao(){
+    //     return exampleDao;
+    // }
+}
