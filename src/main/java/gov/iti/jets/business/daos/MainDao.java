c70a27089f4d135b65ee043a448c976f685efa28
amiraElmergawy <amiraelmergawy@gmail.com>
2022-03-19 12:24:43 +0200

    Update MainDao.java

--- src/main/java/gov/iti/jets/business/daos/MainDao.java
+++ src/main/java/gov/iti/jets/business/daos/MainDao.java
@@ -3,7 +3,7 @@ package gov.iti.jets.business.daos;
 import java.util.Collection;
 import java.util.Optional;
 
-public interface MainDao<T> {
+public interface MainDao<T> {//CRUDDao
     Optional<T> get(int id);
     Collection<T> getAll();
     int save(T t);
